2f5c80e61ed72c6c6412ee57ab00a509267600dd
remo <riedlkovacsj@gmail.com>
2023-04-24 16:33:53 +0200

    referenceSpeed implementation

--- train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainSensor.java
+++ train-interfaces/src/main/java/hu/bme/mit/train/interfaces/TrainSensor.java
@@ -14,4 +14,38 @@ public interface TrainSensor {
     Table<LocalTime, Integer, Integer> getTacho();
 
 	void refreshTacho();
+
+	void trainTimer();
+
+	class Timer implements Runnable {
+		TrainController controller;
+
+		public Timer(TrainController controller){
+			this.controller = controller;
+		}
+
+		private long currentTime = 0;
+
+		@Override
+		public void run(){
+			int time = 0;
+			while (time < 7){
+				try{
+					Thread.sleep(1000);
+
+				}
+				catch (InerruptedException e){
+					throw new RuntimeException(e);				
+				}
+			}
+			controller.followSpeed();
+			currentTime++;
+			time++;
+		}
+
+		public void getTime(){
+			return currentTime;
+		}
+
+	}
 }
